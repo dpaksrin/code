cf1ad1cf93504c379b4825d33b6cccc9348655b5
dpaksrin <dpaksrin@gmail.com>
2016-11-20 17:07:40 -0500

    Initial Checkin - Project Created

---
+++ BabySitterApp/BabySitterApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BabySitterApp.Startup))]
+namespace BabySitterApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
